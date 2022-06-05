3e466f206a2212437986bd03cedb588f1b3ebf40
Esfekia <sertac@gmail.com>
2022-06-05 06:29:11 -0400

    Using the MemoryCard script, we manipulate the Sprite aspect of the SpriteRenderer component attached to the card on screen.
    Using the MemoryCard script, we manipulate the Sprite aspect of the SpriteRenderer component attached to the card on screen.

--- Assets/Scripts/MemoryCard.cs
+++ Assets/Scripts/MemoryCard.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class MemoryCard : MonoBehaviour
 {
+    // variable that appears in the inspector
+    [SerializeField] GameObject cardBack;
+
+    // reference to the sprite asset that will be loaded
+    [SerializeField] Sprite image;
     public void OnMouseDown()
     {
-        Debug.Log("testing 1 2 3");
+        // run deactivate code onlyu if the object is currently active/visible
+        if (cardBack.activeSelf)
+        {
+            // set the object to inactive/invisible
+            cardBack.SetActive(false);
+        }
     }
-
-    // Start is called before the first frame update
+        
     void Start()
     {
-        
+        // set the sprite for this SpriteRenderer component.
+        GetComponent<SpriteRenderer>().sprite = image;
     }
-
-    // Update is called once per frame
+        
     void Update()
     {
         
